05bc419de66ff1031d90e4cbadf42e3195b0cdb1
Sinka Tibor <sinkatibor2004@gmail.com>
2024-11-26 01:59:22 +0100

    Tidy ups #7 -GameUI

--- src/main/java/prog/hazi/ui/GameUI.java
+++ src/main/java/prog/hazi/ui/GameUI.java
@@ -16,16 +16,26 @@ public class GameUI extends JPanel {
     private Team currentPlayer;
 
     private JPanel boardPanel;
-    private JPanel labelPanel;
     private JLabel statusLabel;
 
     private ImageIcon ballIcon;
     private Color tableColor = new Color(207, 175, 99);
     
+    /**
+     * Constructs a new GameUI instance with the specified settings.
+     *
+     * @param st the settings object containing the board size and ball count
+     */
     public GameUI(Settings st) {
         this(st.getBoardSize(), st.getBallCount());
     }
 
+    /**
+     * Constructs a new GameUI with the specified board size and ball count.
+     *
+     * @param boardSize the size of the board
+     * @param ballCount the number of balls
+     */
     public GameUI(int boardSize, int ballCount) {
         board = new Board(boardSize, ballCount); 
         currentPlayer = Team.NORTH;
@@ -37,8 +47,11 @@ public class GameUI extends JPanel {
         initializeUI();
     }
 
+    /**
+     * Reinitializes the game UI by calling the methods to initialize the UI components
+     * and update the game board.
+     */
     public void reinitialize() {
-        // Reinitialize the UI components and event listeners
         initializeUI();
         updateBoard();
     }
@@ -47,7 +60,6 @@ public class GameUI extends JPanel {
         setLayout(new BorderLayout());
         setBackground(currentPlayer.getBgColor());
 
-        // Create and add the table panel
         TablePanel tablePanel = new TablePanel(tableColor, 0.03);
         tablePanel.setOpaque(false);
         add(tablePanel, BorderLayout.CENTER);
@@ -57,88 +69,100 @@ public class GameUI extends JPanel {
         tablePanel.setLayout(new BorderLayout());
         tablePanel.add(boardPanel, BorderLayout.CENTER);
 
-        statusLabel = new JLabel("Welcome to Mancala!");
-
-        labelPanel = new GameLabelPanel(statusLabel);
-
+        statusLabel = new JLabel(currentPlayer.getName() + " can start the game!");
+        setLabelStyle(statusLabel);
+        JPanel labelPanel = new GameLabelPanel(statusLabel);
         add(labelPanel, BorderLayout.NORTH);
 
         updateBoard();
     }
 
+    /**
+     * Updates the game board UI by removing all existing components and 
+     * re-adding them in the correct layout. This method sets up the board 
+     * with the current state of the game, including the store buttons and pits 
+     * for both teams. It also updates the background color based on the current player.
+     */
     public void updateBoard() {
         boardPanel.removeAll();
         boardPanel.setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.fill = GridBagConstraints.BOTH;
-    
-        // South store button
-        JButton storeButtonSouth = createHoleButton(board.getHole(Team.SOUTH, 0).getBallCount(), Team.SOUTH, 12, 3);
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        gbc.gridheight = 4;
-        gbc.insets = new Insets(0, 5, 0, 5);
-        boardPanel.add(storeButtonSouth, gbc);
-    
-        // South pits
-        gbc.gridheight = 1;
-        for (int i = board.getBoardSize(); i > 0; i--) {
-            //JButton pitButton = new JButton(String.valueOf(board.getHole(Team.SOUTH, i).getBallCount()));
-            JButton pitButton = createHoleButton(board.getHole(Team.SOUTH, i).getBallCount(), Team.SOUTH, 5, 3);
-            pitButton.addActionListener(new ButtonListener(i, Team.SOUTH));
-            gbc.gridx = board.getBoardSize() -i + 1;
-            gbc.gridy = 0;
-            gbc.insets = new Insets(0, 5, 0, 5);
-            pitButton.setForeground(Color.BLUE);
-            boardPanel.add(pitButton, gbc);
 
-            JLabel pitCountLabel = new JLabel(String.valueOf(board.getHole(Team.SOUTH, i).getBallCount()));
-            pitCountLabel.setHorizontalAlignment(SwingConstants.CENTER);
-            pitCountLabel.setVerticalAlignment(SwingConstants.TOP);
-            pitCountLabel.setFont(new Font("Roboto", Font.BOLD, 16));
-            gbc.gridy = 1;
-            gbc.insets = new Insets(0, 0, 20, 0);
-            boardPanel.add(pitCountLabel, gbc);
-        }
-    
-        // North pits
-        for (int i = 0; i < board.getBoardSize(); i++) {
-            //JButton pitButton = new JButton(String.valueOf(board.getHole(Team.NORTH, i + 1).getBallCount()));
-            JButton pitButton = createHoleButton(board.getHole(Team.NORTH, i + 1).getBallCount(), Team.NORTH, 5, 3);
-            pitButton.addActionListener(new ButtonListener(i + 1, Team.NORTH));
-            gbc.gridx =  i + 1;
-            gbc.gridy = 3;
-            gbc.insets = new Insets(0, 5, 0, 5);
-            pitButton.setForeground(Color.RED);
-            boardPanel.add(pitButton, gbc);
+        addStoreButton(gbc, Team.SOUTH, 0, 0, 4);
+        addPits(gbc, Team.SOUTH, 0, 1, true);
+        addPits(gbc, Team.NORTH, 3, 2, false);
+        addStoreButton(gbc, Team.NORTH, board.getBoardSize() + 1, 0, 4);
 
-            
-            JLabel pitCountLabel = new JLabel(String.valueOf(board.getHole(Team.NORTH, i+1).getBallCount()));
-            pitCountLabel.setHorizontalAlignment(SwingConstants.CENTER);
-            pitCountLabel.setVerticalAlignment(SwingConstants.BOTTOM);
-            pitCountLabel.setFont(new Font("Roboto", Font.BOLD, 16));
-            gbc.gridy = 2;
-            gbc.insets = new Insets(20, 5, 0, 5);
-            boardPanel.add(pitCountLabel, gbc);
-        }
-
-        // North store button
-        JButton storeButtonNorth = createHoleButton(board.getHole(Team.NORTH, 0).getBallCount(), Team.NORTH, 12, 3);
-        gbc.gridx = board.getBoardSize() + 1;
-        gbc.gridy = 0;
-        gbc.gridheight = 4;
-        gbc.insets = new Insets(0, 5, 0, 5);
-        boardPanel.add(storeButtonNorth, gbc);
-        //disable the store buttons
-        storeButtonNorth.setEnabled(false);
-        storeButtonSouth.setEnabled(false);
-        // storeButtonNorth.setForeground(Color.RED);
-        // storeButtonSouth.setForeground(Color.BLUE);
-    
         setBackground(currentPlayer.getBgColor());
 
         boardPanel.revalidate();
         boardPanel.repaint();
+    }
+
+    /**
+     * Adds a store button to the game UI.
+     *
+     * @param gbc         the GridBagConstraints used to layout the button
+     * @param team        the team to which the store belongs
+     * @param gridx       the x-coordinate of the button in the grid
+     * @param gridy       the y-coordinate of the button in the grid
+     * @param gridheight  the height of the button in grid cells
+     */
+    private void addStoreButton(GridBagConstraints gbc, Team team, int gridx, int gridy, int gridheight) {
+        JButton storeButton = createHoleButton(board.getHole(team, 0).getBallCount(), team, 12, 3);
+        gbc.gridx = gridx;
+        gbc.gridy = gridy;
+        gbc.gridheight = gridheight;
+        gbc.insets = new Insets(0, 5, 0, 5);
+        boardPanel.add(storeButton, gbc);
+    }
+
+    /**
+     * Adds pits to the game board UI for a specified team.
+     *
+     * @param gbc       the GridBagConstraints used for layout management
+     * @param team      the team for which the pits are being added (either SOUTH or NORTH)
+     * @param pitsY     the y-coordinate for the pits in the grid layout
+     * @param labelY    the y-coordinate for the labels in the grid layout
+     * @param reverse   if true, pits are added in reverse order; otherwise, they are added in normal order
+     */
+    private void addPits(GridBagConstraints gbc, Team team, int pitsY, int labelY, boolean reverse) {
+        gbc.gridheight = 1;
+        int start = reverse ? board.getBoardSize() : 1;
+        int end = reverse ? 0 : board.getBoardSize() + 1;
+        int step = reverse ? -1 : 1;
+
+        for (int i = start; i != end; i += step) {
+            JButton pitButton = createHoleButton(board.getHole(team, i).getBallCount(), team, 5, 3);
+            pitButton.addActionListener(new ButtonListener(i, team));
+            gbc.gridx = reverse ? board.getBoardSize() - i + 1 : i;
+            gbc.gridy = pitsY;
+            gbc.insets = new Insets(0, 5, 0, 5);
+            pitButton.setForeground(team == Team.SOUTH ? Color.BLUE : Color.RED);
+            boardPanel.add(pitButton, gbc);
+
+            JLabel pitCountLabel = new JLabel(String.valueOf(board.getHole(team, i).getBallCount()));
+            pitCountLabel.setHorizontalAlignment(SwingConstants.CENTER);
+            setLabelStyle(pitCountLabel);
+            gbc.gridy = labelY;
+            gbc.insets = new Insets(0, 0, reverse ? 30 : 0, 0);
+            boardPanel.add(pitCountLabel, gbc);
+        }
+    }
+
+    /**
+     * Sets the style for the given JLabel.
+     * This includes setting the vertical alignment to center,
+     * the font to "Alexandria" with bold style and size 15,
+     * and the foreground (Text) color to black.
+     *
+     * @param label the JLabel to style
+     */
+    private void setLabelStyle(JLabel label) {
+        label.setVerticalAlignment(SwingConstants.CENTER);
+        label.setFont(new Font("Alexandria", Font.BOLD, 15));
+        label.setForeground(Color.BLACK);
     }
 
     private JButton createHoleButton(int ballCount, Team t, int cols, int rows) {
@@ -186,18 +210,38 @@ public class GameUI extends JPanel {
     }
 
 
+    /**
+     * Sets the board for the game.
+     *
+     * @param board the {@link Board} object to be set
+     */
     public void setBoard(Board board) {
         this.board = board;
     }
 
+    /**
+     * Sets the current player to the specified team.
+     *
+     * @param t the {@link Team} to set as the current player
+     */
     public void setCurrentPlayer(Team t) {
         currentPlayer = t;
     }
 
+    /**
+     * Retrieves the current game board.
+     *
+     * @return the current instance of the {@link Board}.
+     */
     public Board getBoard() {
         return board;
     }
 
+    /**
+     * Returns the current player.
+     *
+     * @return the current player as a {@link Team} object.
+     */
     public Team getCurrentPlayer() {
         return currentPlayer;
     }
